f18d12843d31c9f4d9d52829a8a2760109ba9fe0
P4t4m8n <you@example.com>
2024-01-01 13:45:42 +0200

    Change upload func

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-    // dbURL: 'mongodb://127.0.0.1:27017',
-    dbURL: 'mongodb+srv://P4t4m8n:vKdtkapl5Xk7dCOl@p4t4server.7ovkpa1.mongodb.net/?retryWrites=true&w=majority',
+    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb+srv://P4t4m8n:YzVTNJwhgpopMeNS@p4t4server.7ovkpa1.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'toys_db',
 }
