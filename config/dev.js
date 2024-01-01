751686103f89bcef41c2540357e3dad84692128f
P4t4m8n <you@example.com>
2024-01-01 13:16:33 +0200

    Update for db on render

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://P4t4m8n:vKdtkapl5Xk7dCOl@p4t4server.7ovkpa1.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'toys_db',
 }
