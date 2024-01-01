02d2da5789aaeba2d8c6662e0e1222c746282aa1
P4t4m8n <you@example.com>
2024-01-01 14:45:47 +0200

    Test

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: process.env.MONGO_URL|| 'mongodb+srv://P4t4m8n:vKdtkapl5Xk7dCOl@p4t4server.7ovkpa1.mongodb.net/?retryWrites=true&w=majority',
+    dbURL:  'mongodb+srv://P4t4m8n:YzVTNJwhgpopMeNS@p4t4server.7ovkpa1.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'toy_db',
 }
